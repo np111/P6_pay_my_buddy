4f0d3e2a2156af54b55a70a84df4a39b8cf33d06
np111 <oc.np111@protonmail.com>
2020-12-14 16:21:36 +0100

    Add default popover translations

--- client/src/components/ui/popconfirm.tsx
+++ client/src/components/ui/popconfirm.tsx
@@ -1,6 +1,10 @@
 import AntPopconfirm, {PopconfirmProps as AntPopconfirmProps} from 'antd/lib/popconfirm';
 import 'antd/lib/popconfirm/style/index.less';
 import 'antd/lib/popover/style/index.less';
+import React from 'react';
+import {WithTranslation, withTranslation} from '../i18n';
 
 export type PopconfirmProps = AntPopconfirmProps;
-export const Popconfirm = AntPopconfirm; // TODO: Default okText/cancelText based on locale
+export const Popconfirm = withTranslation('common')(function Popconfirm({t, ...props}: PopconfirmProps & WithTranslation) {
+    return <AntPopconfirm okText={t('common:intl.ok')} cancelText={t('common:intl.cancel')} {...props}/>;
+});
